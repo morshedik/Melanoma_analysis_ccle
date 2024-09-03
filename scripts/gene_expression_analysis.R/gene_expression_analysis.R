ca803b5f9d69cad95ab1772cc304906849587306
Ko Mo <morshedik@gmail.com>
2024-09-03 13:07:25 -0400

    complete and specify gene_expression file

--- scripts/gene_expression_analysis.R/gene_expression_analysis.R
+++ scripts/gene_expression_analysis.R/gene_expression_analysis.R
@@ -2,10 +2,6 @@
 ccle_a375 <- ccle_expression %>%
   filter(str_detect(cell_line, "A375"))
 
-# Filter for A375 in GDSC
-gdsc_a375 <- gdsc_response %>%
-  filter(str_detect(CELL_LINE_NAME, "A375"))
-print(gdsc_a375)
 
 
 # Analyze gene expression data for A375
@@ -17,17 +13,20 @@ print(head(gene_expression_a375, 20))
 
 
 
-# Analyze drug response data for A375
-drug_response_a375 <- gdsc_a375 %>%
-  arrange(LN_IC50) %>%
-  select(DRUG_NAME, PUTATIVE_TARGET, LN_IC50, AUC)
-print("Top 20 most effective drugs on A375 (based on lowest LN_IC50):")
-print(head(drug_response_a375, 20))
-
-
 # Save results
 output_direct <- "~/Desktop/R_output/Final_data"
 dir.create(output_direct, showWarnings = FALSE)
 write_csv(gene_expression_a375, file.path(output_direct,"a375_gene_expression.csv"))
-write_csv(drug_response_a375, file.path(output_direct,"a375_drug_response.csv"))
+
+
+# Basic statistical summary of gene expression
+gene_expression_summary <- summary(ccle_a375$rna_expression)
+print("Summary of A375 gene expression:")
+print(gene_expression_summary)
+
+# Visualize gene expression distribution
+pdf("~/Desktop/R_output/Final_data/a375_gene_expression_histogram.pdf")
+hist(ccle_a375$rna_expression, main="Distribution of Gene Expression in A375", xlab="RNA Expression")
+dev.off()
+
 
